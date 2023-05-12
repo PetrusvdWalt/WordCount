fef2dd5c699b5dc297c97bf54e036039b6dc6235
Petrus van der Walt <Petrus.vanderWalt@standardbank.co.za>
2023-05-12 07:33:00 +0200

    Basic project setup

---
+++ src/test/java/inteviews/ordinia/WordCount/WordCountApplicationTests.java
@@ -0,0 +1,13 @@
+package inteviews.ordinia.WordCount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordCountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
